5c839cb86289b2fb837f2220069282ef42fd561b
thejaswi <yourstheja166@gmail.com>
2026-01-10 09:40:03 +0530

    first

---
+++ src/main/java/com/example/duoclone/DuocloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.duoclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuocloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuocloneApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/duoclone/model/UserProgress.java
@@ -0,0 +1,4 @@
+package com.example.duoclone.model;
+
+public class UserProgress {
+}
